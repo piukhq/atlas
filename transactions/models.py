7867b3ae2fee330b16298b4c6d50c0bc1db3fe08
Chris Toal <ctoal@bink.com>
2018-12-06 11:55:13 +0000

    Initial commit - Added transactions app and initialised admin interface

---
+++ transactions/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+
+
+class Transaction(models.Model):
+    """
+    Transaction export status
+    """
+    created_date = models.DateTimeField(auto_now_add=True, db_index=True)
+    scheme_provider = models.CharField(max_length=100, db_index=True)
+    response = models.CharField(max_length=3000, blank=True)
+    transaction_id = models.CharField(max_length=100, db_index=True, unique=True)
+    status = models.CharField(max_length=50, db_index=True)
+    transaction_date = models.DateTimeField(blank=True)
+    user_id = models.CharField(max_length=30, blank=True)
+    amount = models.IntegerField(blank=True)
+
+    def __unicode__(self):
+        return self.created_date
