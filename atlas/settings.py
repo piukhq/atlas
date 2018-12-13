76a79028710e089d08f20df8c8811aada74e3dee
Chris Toal <ctoal@bink.com>
2018-12-13 16:49:45 +0000

    updated settings to use postgres

--- atlas/settings.py
+++ atlas/settings.py
@@ -95,8 +95,13 @@ WSGI_APPLICATION = 'atlas.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': env_var("ATLAS_DATABASE_NAME", "atlas"),
+        'USER': env_var("ATLAS_DATABASE_USER", "postgres"),
+        'PASSWORD': env_var("ATLAS_DATABASE_PASS"),
+        'HOST': env_var("ATLAS_DATABASE_HOST", "localhost"),
+        'PORT': env_var("ATLAS_DATABASE_PORT", "5432"),
+        'CONN_MAX_AGE': None,  # unlimited persistent connections
     }
 }
 
