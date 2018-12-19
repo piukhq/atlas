71ce42a28f46c1f354e0b2ca8d30a317c504fe97
Chris Toal <ctoal@bink.com>
2018-12-19 09:59:38 +0000

    Cleaned up exceptions and logging after testing

--- transactions/views.py
+++ transactions/views.py
@@ -5,6 +5,7 @@ import json
 
 from azure.common import AzureException
 from django.core import serializers
+from django.db import IntegrityError
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
@@ -16,7 +17,7 @@ from transactions.storage import create_blob_from_json
 
 
 class TransactionBlobView(APIView):
-    """View to handle incoming transaction data from Aphrodite"""
+    """View to query Transaction database and save result to blob storage"""
     queryset = Transaction.objects.all()
     serializer_class = TransactionSerializer
 
@@ -27,41 +28,59 @@ class TransactionBlobView(APIView):
         start = request.data['start']
         end = request.data['end']
 
-        transactions = get_transactions(start, end)
+        try:
+            transactions = get_transactions(start, end)
+
+        except (ValueError, TypeError) as e:
+            logger.exception(
+                'TransactionBlobView: Date must reflect YYYY-MM-DD format: {}'.format(e.args[0]))
+            return Response(data='Date must reflect YYYY-MM-DD format: {}'.format(e.args[0]), status=400)
+
         trans = json.loads(transactions)
 
         if not trans:
-            logger.info('No transactions between these dates: {}--{}'.format(start, end))
-            return Response(data='No transactions between these dates: {}--{}'.format(start, end), status=400)
+            logger.info('TransactionBlobView: No transactions between these dates: {}--{}'.format(start, end))
+            return Response(data='No transactions between these dates: {}--{}'.format(start, end), status=204)
 
         try:
             create_blob_from_json(transactions, scheme_slug=trans[0]['fields']['scheme_provider'])
+
         except AzureException as e:
+            logger.exception('TransactionBlobView: Error saving to Blob storage - {} data - {}'.format(e, trans))
             return Response(
-                data='TransactionBlobView: Error saving to blob storage - {}  ::: data = {}'.format(e, trans),
+                data='Error saving to blob storage - {} data - {}'.format(e, trans),
                 status=e.status_code)
+
+        except ValueError as e:
+            logger.exception(
+                'TransactionBlobView: Error saving to Blob storage - {} data - {}'.format(e.args[0], trans))
+            return Response(
+                data='Error saving to blob storage - {} data - {}'.format(e.args[0], trans),
+                status=520)
 
         return Response(data=trans, status=200)
 
 
 class TransactionSaveView(APIView):
+    """View to handle incoming transaction data from Aphrodite and save to postgres"""
 
     @staticmethod
     @token_check
     def post(request):
-        return save_transaction(request.data)
+        try:
+            transaction = save_transaction(request.data)
+            return transaction
+
+        except (IntegrityError, KeyError, Exception) as e:
+            logger.exception('Error saving transaction: {}'.format(e.args[0]))
+            return Response(data="Transaction not saved: {}".format(e.args[0]), status=400)
 
 
 def get_transactions(start_date, end_date):
     format_str = '%Y-%m-%d'
 
-    try:
-        start_datetime = datetime.datetime.strptime(start_date, format_str)
-        end_datetime = datetime.datetime.strptime(end_date, format_str) + datetime.timedelta(days=1)
-
-    except ValueError:
-        logger.info('Error retrieving transactions : Date must reflect YYYY-MM-DD format: {}'.format(ValueError))
-        return Response(data='Date must reflect YYYY-MM-DD format', status=400)
+    start_datetime = datetime.datetime.strptime(start_date, format_str)
+    end_datetime = datetime.datetime.strptime(end_date, format_str) + datetime.timedelta(days=1)
 
     transactions = Transaction.objects.filter(created_date__range=(start_datetime, end_datetime))
     serialized_transaction = serializers.serialize('json', transactions)
@@ -80,4 +99,4 @@ def save_transaction(transaction_data):
         amount=transaction_data['amount']
     )
     transaction.save()
-    return Response(data='Transaction saved', status=201)
+    return Response(data='Transaction saved: {}'.format(transaction), status=201)
