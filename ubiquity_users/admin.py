24b1cd348f559309b89775ff24e6f5473ed612f0
Chris Toal <ctoal@bink.com>
2019-01-17 11:04:15 +0000

    Changed fields to read only

--- ubiquity_users/admin.py
+++ ubiquity_users/admin.py
@@ -3,5 +3,6 @@ from django.contrib import admin
 from ubiquity_users.models import User
 
 
-admin.site.register(User)
-
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    readonly_fields = ['email', 'opt_out_timestamp', 'time_added_to_database']
