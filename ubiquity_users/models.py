137cb9a78f80013a3cc15b23277fa89a3ad75fa9
Chris Toal <ctoal@bink.com>
2019-02-13 16:33:09 +0000

    Changed field name User model opt_out_field to ubiquity_join_date

--- ubiquity_users/models.py
+++ ubiquity_users/models.py
@@ -3,11 +3,11 @@ from django.db import models
 
 class User(models.Model):
     """
-    Barclays users who have opted out of bink
+    Barclays users who have opted out of ubiquity
     """
     time_added_to_database = models.DateTimeField(auto_now_add=True, db_index=True, blank=False)
     email = models.EmailField(max_length=100, blank=False)
-    opt_out_timestamp = models.DateTimeField(blank=False)
+    ubiquity_join_date = models.DateTimeField(blank=False)
     delete = models.BooleanField(default=False)
 
     def __unicode__(self):
