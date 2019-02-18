52b58b37a7da6e679608f5d825b4c7f28489981c
Chris Toal <ctoal@bink.com>
2019-02-18 12:02:34 +0000

    Updated admin time added to database field to include 'opt out timestamp'

--- ubiquity_users/models.py
+++ ubiquity_users/models.py
@@ -1,11 +1,14 @@
 from django.db import models
+from django.utils.html import format_html
 
 
 class User(models.Model):
     """
     Barclays users who have opted out of ubiquity
     """
-    time_added_to_database = models.DateTimeField(auto_now_add=True, db_index=True, blank=False)
+    time_added_to_database = models.DateTimeField(auto_now_add=True, db_index=True, blank=False,
+                                                  verbose_name=format_html(
+                                                      "Opt out timestamp <br> Time added to database"))
     email = models.EmailField(max_length=100, blank=False)
     ubiquity_join_date = models.DateTimeField(blank=False)
     delete = models.BooleanField(default=False)
