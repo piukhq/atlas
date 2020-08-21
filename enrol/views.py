bad186a1561e0cf2f60123e05554a58b15ff705f
Tony Truong <ttruong@bink.com>
2020-08-21 17:47:06 +0100

    lint fix.

--- enrol/views.py
+++ enrol/views.py
@@ -4,8 +4,6 @@ from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
-from atlas.settings import logger
-from enrol.models import EnrolRequest
 from enrol.serializers import EnrolRequestSerializer
 from enrol.authentication import ServiceAuthentication
 
