2b00e98580cefdcede39ec5febb16a911eda43df
backwardspy <backwardspy@pigeon.life>
2024-01-18 11:00:08 +0000

    feat: make scheme fields optional in channel table

--- membership/models.py
+++ membership/models.py
@@ -44,8 +44,8 @@ class UserChannelIdentifier(models.Model):
     date_joined = models.DateTimeField(help_text="User created date")
     bundle_id = models.TextField(help_text="Channel identifier")
     created_link = models.DateTimeField(null=True, help_text="Creation date of user to scheme account association")
-    scheme_account_id = models.IntegerField()
-    created_scheme_account = models.DateTimeField(help_text="Scheme account created date")
+    scheme_account_id = models.IntegerField(null=True)
+    created_scheme_account = models.DateTimeField(null=True, help_text="Scheme account created date")
     join_date = models.DateTimeField(null=True, help_text="Scheme account join date (if applicable)")
     link_date = models.DateTimeField(null=True, help_text="Scheme account link date (if applicable)")
     card_number = models.TextField(blank=True, null=True)
@@ -53,9 +53,9 @@ class UserChannelIdentifier(models.Model):
     alt_main_answer = models.TextField(blank=True, null=True)
     merchant_identifier = models.TextField(blank=True, null=True)
     originating_journey = models.IntegerField(
-        help_text="Journey type ID (Join = 0, Link = 1, Add = 2, Update = 3, Register = 4, Unknown = 5)"
+        null=True, help_text="Journey type ID (Join = 0, Link = 1, Add = 2, Update = 3, Register = 4, Unknown = 5)"
     )
-    scheme_id = models.IntegerField()
+    scheme_id = models.IntegerField(null=True)
 
     def __str__(self):
         return f"UserChannelIdentifier - User {self.user_id} in channel {self.bundle_id}"
